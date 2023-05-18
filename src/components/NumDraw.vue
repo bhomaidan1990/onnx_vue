1ad5d30e600fbc691520db0e67b4bd7ace82e982
Belal_Alienware <bhomaidan@hotmail.com>
2023-05-18 08:29:54 +0200

    fix bugs

--- src/components/NumDraw.vue
+++ src/components/NumDraw.vue
@@ -30,8 +30,8 @@ import PredCol from "./PredCol.vue";
 
 export default {
   name: "NumDraw",
-  data(){
-    return{
+  data() {
+    return {
       isMouseDown: false,
       hasIntroText: true,
       lastX: 0,
@@ -39,27 +39,33 @@ export default {
       CANVAS_SIZE: 280,
       CANVAS_SCALE: 0.5,
       ctx: null,
+      sess: null,
     };
   },
   components: {
     PredCol,
   },
 
-  mounted(){
+  mounted() {
     this.init();
   },
 
   methods: {
-    init() {
+    async init() {
       const canvas = document.getElementById("canvas");
       this.ctx = canvas.getContext("2d");
 
       const clearButton = document.getElementById("clear-button");
 
       // Load our model.
-      const sess = ort.InferenceSession;
-      const sessionOption = {executionProviders: [{name: 'webgl', deviceId: 0}]};
-      const loadingModelPromise = sess.create("./models/mnist.onnx", sessionOption);
+      const InferenceSession = ort.InferenceSession;
+      const sessionOption = {
+        executionProviders: [{ name: "webgl", deviceId: 0 }],
+      };
+      this.sess = await InferenceSession.create(
+        "./models/mnist.onnx",
+        sessionOption
+      );
 
       // Add 'Draw a number here!' to the canvas.
       this.ctx.lineWidth = 28;
@@ -69,23 +75,29 @@ export default {
       this.ctx.textBaseline = "middle";
       this.ctx.fillStyle = "#212121";
       // this.ctx.fillText("Draw a number here!", this.CANVAS_SIZE / 2, this.CANVAS_SIZE / 2);
-      this.ctx.fillText("Loading...", this.CANVAS_SIZE / 2, this.CANVAS_SIZE / 2);
+      this.ctx.fillText(
+        "Loading...",
+        this.CANVAS_SIZE / 2,
+        this.CANVAS_SIZE / 2
+      );
 
       // Set the line color for the canvas.
       this.ctx.strokeStyle = "#212121";
 
-      loadingModelPromise.then(() => {
-        canvas.addEventListener("mousedown", this.canvasMouseDown);
-        canvas.addEventListener("mousemove", this.canvasMouseMove);
-        document.body.addEventListener("mouseup", this.bodyMouseUp);
-        document.body.addEventListener("mouseout", this.bodyMouseOut);
-        clearButton.addEventListener("mousedown", this.clearCanvas);
+      canvas.addEventListener("mousedown", this.canvasMouseDown);
+      canvas.addEventListener("mousemove", this.canvasMouseMove);
+      document.body.addEventListener("mouseup", this.bodyMouseUp);
+      document.body.addEventListener("mouseout", this.bodyMouseOut);
+      clearButton.addEventListener("mousedown", this.clearCanvas);
 
-        this.ctx.clearRect(0, 0, this.CANVAS_SIZE, this.CANVAS_SIZE);
-        this.ctx.fillText("Draw a number here!", this.CANVAS_SIZE / 2, this.CANVAS_SIZE / 2);
-      });
+      this.ctx.clearRect(0, 0, this.CANVAS_SIZE, this.CANVAS_SIZE);
+      this.ctx.fillText(
+        "Draw a number here!",
+        this.CANVAS_SIZE / 2,
+        this.CANVAS_SIZE / 2
+      );
     },
-    
+
     clearCanvas() {
       this.ctx.clearRect(0, 0, this.CANVAS_SIZE, this.CANVAS_SIZE);
       for (let i = 0; i < 10; i++) {
@@ -107,13 +119,18 @@ export default {
 
     async updatePredictions() {
       // Get the predictions for the canvas data.
-      const imgData = this.ctx.getImageData(0, 0, this.CANVAS_SIZE, this.CANVAS_SIZE);
-      let img_arr= new Float32Array(imgData.data); //560 x 560 = 313600
-      const dims = [1, 1, 280, 280];
-      console.log("Image", img_arr)
-      const input = new ort.Tensor(img_arr, "float32", dims);
+      const imgData = this.ctx.getImageData(
+        0,
+        0,
+        this.CANVAS_SIZE,
+        this.CANVAS_SIZE
+      );
+      let img_arr = new Float32Array(imgData.data); //560 x 560 = 313600
+      const dims = [1, 1, 560, 560];
+      let input_tensor = new ort.Tensor("float32", img_arr, dims);
 
-      const outputMap = await sess.run([input]);
+      const feeds = { a: input_tensor };
+      const outputMap = await this.sess.run(feeds);
       const outputTensor = outputMap.values().next().value;
       const predictions = outputTensor.data;
       const maxPrediction = Math.max(...predictions);
